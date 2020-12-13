644a9200c7ee8415b8167759229489b7cc7af511
vaseekaran <vasee002@gmail.com>
2020-12-13 09:18:15 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { from } from 'rxjs';
+import {HeaderComponent} from './components/header/header.component';
+import { LoginComponent} from './components/login/login.component';
+import { RegisterComponent} from './components/register/register.component';
+import { ResetPasswordComponent} from './components/reset-password/reset-password.component';
+import {  PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+const routes: Routes = [
+  {path: '',redirectTo : 'header',pathMatch : 'full'},
+  {path: 'header',component: HeaderComponent },
+  {path : 'login',component : LoginComponent},
+  {path : 'register',component :RegisterComponent},
+  {path : 'reset-password',component :ResetPasswordComponent},
+  {path : '**',component :PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const AppRoutingComponents = [LoginComponent,RegisterComponent,ResetPasswordComponent,PageNotFoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { AppRoutingModule,AppRoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { SlideBarComponent } from './components/slide-bar/slide-bar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {SidebarModule} from 'ng-sidebar';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRoutingComponents,
+    HeaderComponent,
+    SlideBarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    PageNotFoundComponent,
+   
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    SidebarModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
